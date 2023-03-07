52056f30dad79faf6b3aa71666134095560788cf
Kingmodu <120216464+Kingmodu@users.noreply.github.com>
2023-03-07 11:03:18 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+password = input ('enter password ')
+if password == ('mana'):
+    print ('vishwainc.com')
+elif password == '8764':
+	print ('vishwainc.com')
+else:
+	print ('you dont have access')
+	print ('you cant get in without knowing the password')
+	print ('dont try hacking')
+	print ('you can get in if your smart')
